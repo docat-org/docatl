b4ae4c15e91d38d845c7a3ede1933f61bdfbceef
Timo Furrer <tuxtimo@gmail.com>
2021-10-19 15:05:00 +0200

    Fix file mode for new config files

--- pkg/config.go
+++ pkg/config.go
@@ -18,7 +18,7 @@ func WriteConfig(configPath string, config Config) error {
 		return fmt.Errorf("unable to marshal config '%v' to YAML: %w", config, err)
 	}
 
-	err = ioutil.WriteFile(configPath, doc, 0)
+	err = ioutil.WriteFile(configPath, doc, 0644)
 	if err != nil {
 		return fmt.Errorf("unable to write config to '%s': %w", configPath, err)
 	}
